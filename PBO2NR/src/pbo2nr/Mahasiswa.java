8ee6957bb3a1f2ccddacb3aff4f042240a3606b6
Mirza Yogy Kurniawan <mirza.yogy@gmail.com>
2017-09-23 09:40:22 +0800

    Buat class main, tambah method

--- PBO2NR/src/pbo2nr/Mahasiswa.java
+++ PBO2NR/src/pbo2nr/Mahasiswa.java
@@ -1,6 +1,80 @@
 package pbo2nr;
 public class Mahasiswa {
-    int NPM;
+    String NPM;
     String Nama;
     String Alamat;
+    double nilaiHarian;
+    double nilaiTugas;
+    double nilaiUTS;
+    double nilaiUAS;
+    double nilaiAbjad;
+
+    public String getNPM() {
+        return NPM;
+    }
+
+    public void setNPM(String NPM) {
+        this.NPM = NPM;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+
+    public String getAlamat() {
+        return Alamat;
+    }
+
+    public void setAlamat(String Alamat) {
+        this.Alamat = Alamat;
+    }
+
+    public double getNilaiHarian() {
+        return nilaiHarian;
+    }
+
+    public void setNilaiHarian(double nilaiHarian) {
+        this.nilaiHarian = nilaiHarian;
+    }
+
+    public double getNilaiTugas() {
+        return nilaiTugas;
+    }
+
+    public void setNilaiTugas(double nilaiTugas) {
+        this.nilaiTugas = nilaiTugas;
+    }
+
+    public double getNilaiUTS() {
+        return nilaiUTS;
+    }
+
+    public void setNilaiUTS(double nilaiUTS) {
+        this.nilaiUTS = nilaiUTS;
+    }
+
+    public double getNilaiUAS() {
+        return nilaiUAS;
+    }
+
+    public void setNilaiUAS(double nilaiUAS) {
+        this.nilaiUAS = nilaiUAS;
+    }
+
+    public double getNilaiAbjad() {
+        return nilaiAbjad;
+    }
+
+    public void setNilaiAbjad(double nilaiAbjad) {
+        this.nilaiAbjad = nilaiAbjad;
+    }
+    
+    void mengerjakanTugas(){
+        
+    }
+    
 }
---
+++ PBO2NR/src/pbo2nr/main.java
@@ -0,0 +1,11 @@
+package pbo2nr;
+public class main {
+    public static void main(String[] args) {
+        Mahasiswa m1 = new Mahasiswa();
+        m1.Nama = "Adul";
+        m1.NPM = "13639999";
+        m1.Alamat = "Sungai Besar";
+        
+  
+    }
+}

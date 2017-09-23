2c6f9bb2d65e335045bca942c818ebcbab5c4590
Mirza Yogy Kurniawan <mirza.yogy@gmail.com>
2017-09-23 09:55:52 +0800

    Mengisi atribut pada objek

--- PBO2NR/src/pbo2nr/KucingBeraksi.java
+++ PBO2NR/src/pbo2nr/KucingBeraksi.java
@@ -2,5 +2,10 @@ package pbo2nr;
 public class KucingBeraksi {
     public static void main(String[] args) {
         Kucing kucingKu = new Kucing();
+        
+        kucingKu.jenis="Anggora";
+        kucingKu.jumlahKumis = 8;
+        kucingKu.panjangEkor = 15.6;
+        kucingKu.warna = "Jingga";
     }
 }
